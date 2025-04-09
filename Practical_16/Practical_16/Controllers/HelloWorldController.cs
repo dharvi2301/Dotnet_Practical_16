2929f4353d0872b2fd4b5698cf735456ce4ac673
dharvi-kaneriya <dharvikaneriya@gmail.com>
2025-04-09 16:23:27 +0530

    feature: Implemented the Hello World API controller.

---
+++ Practical_16/Practical_16/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practical_16.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Controller - HelloWorld \n\tActionMetod - Get Method called");
+            return Content("Hello World !!");
+        }
+    }
+
+}
+
